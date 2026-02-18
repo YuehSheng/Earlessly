dd58858f2c4e3a1aeed88c45ca944fe94507aa7c
YuehSheng <you@example.com>
2026-02-18 12:09:36 +0800

    add allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Earlessly/',
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: [
+          'localhost',
+          '127.0.0.1',
+          '.ngrok-free.app',
+          'yuehsheng.github.io'
+        ]
       },
       plugins: [react()],
       define: {
